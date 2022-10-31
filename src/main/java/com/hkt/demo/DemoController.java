7c72aaa9795c47cef005e47511a06e3702b79ee9
tonylinchao <tonyclin@126.com>
2022-10-31 14:25:29 +0800

    init commit

---
+++ src/main/java/com/hkt/demo/DemoController.java
@@ -0,0 +1,13 @@
+package com.hkt.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/healthcheck")
+    public String healthcheck() {
+        return "ok";
+    }
+}
