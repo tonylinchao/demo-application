7350fe93378a8ab6ae0f4f54a1c0267b6d96563a
tonylinchao <tonyclin@126.com>
2022-10-31 14:50:28 +0800

    change to get app name from config

--- src/main/java/com/hkt/demo/DemoController.java
+++ src/main/java/com/hkt/demo/DemoController.java
@@ -1,13 +1,16 @@
 package com.hkt.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Value("${app-name}")
+    private String appName;
     @GetMapping("/healthcheck")
     public String healthcheck() {
-        return "Application B is ok.";
+        return appName + " is ok.";
     }
 }
